48e7204fb95939d936101a028cf1152310bc1ed0
victrosantos <victor.santos.fk@hotmail.com>
2022-07-31 11:24:00 -0300

    Correção da estrutura do codigo e feito exer 8

--- bl5/aula3/script.js
+++ bl5/aula3/script.js
@@ -8,25 +8,7 @@ function createDaysOfTheWeek() {
     dayListItem.innerHTML = days;
 
     weekDaysList.appendChild(dayListItem);
-  };
-};
-
-createDaysOfTheWeek();
-
-let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-for (let x in decemberDaysList) {
-  let dia = decemberDaysList[x];
-  days.appendChild(document.createElement('li'));
-  days.children[x].innerHTML = dia;
-  days.children[x].className = 'day';
-  if (dia === 24 || dia === 25 || dia === 31) {
-    days.children[x].className += ' holiday';
   }
-  if (dia === 4 || dia === 11 || dia === 18 || dia === 25) {
-    days.children[x].className += ' friday';
-  }
-  days.children[x].addEventListener('mouseover', zoomin);
-  days.children[x].addEventListener('mouseout', zoomout);
 }
 function feriados() {
   let divBotao = document.querySelector('.buttons-container');
@@ -34,7 +16,7 @@ function feriados() {
   divBotao.children[0].id = 'btn-holiday';
   divBotao.children[0].innerHTML = 'Feriados';
 }
-function trocaCor(){
+function trocaCor() {
   let diasFeriados = document.getElementsByClassName('holiday');
   for (let cor = 0; cor < diasFeriados.length; cor += 1) {
     if (diasFeriados[cor].style.backgroundColor !== 'white') {
@@ -70,23 +52,53 @@ function zoomout(origem) {
   origem.target.style.fontSize = '20px';
   origem.target.style.fontWeight = '';
 }
-function adicionaEvento(origem) {
-  let caixinha = document.querySelector('#task-input');
-  let tarefas = document.querySelector('.my-tasks');
+function adicionaEvento() {
   if (caixinha.value !== '') {
     tarefas.appendChild(document.createElement('span'));
     tarefas.children[tarefas.children.length - 1].innerHTML = caixinha.value;
   }
 }
+function legendaCor(cor) {
+  tarefas.appendChild(document.createElement('div'));
+  let elemento = tarefas.children[tarefas.children.length - 1];
+  elemento.className = 'task';
+  elemento.style.backgroundColor = cor;
+}
 
 
+
+let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+for (let x in decemberDaysList) {
+  let dia = decemberDaysList[x];
+  days.appendChild(document.createElement('li'));
+  days.children[x].innerHTML = dia;
+  days.children[x].className = 'day';
+  if (dia === 24 || dia === 25 || dia === 31) {
+    days.children[x].className += ' holiday';
+  }
+  if (dia === 4 || dia === 11 || dia === 18 || dia === 25) {
+    days.children[x].className += ' friday';
+  }
+  days.children[x].addEventListener('mouseover', zoomin);
+  days.children[x].addEventListener('mouseout', zoomout);
+}
+
+createDaysOfTheWeek();
 feriados();
 sextou();
+
 let botaoFeriado = document.querySelector('.buttons-container').children[0];
 let botaoSextou = document.querySelector('.buttons-container').children[1];
 let botaoEvento = document.querySelector('#btn-add');
+let caixinha = document.querySelector('#task-input');
+let tarefas = document.querySelector('.my-tasks');
+
+legendaCor('green')
 
 botaoFeriado.addEventListener('click', trocaCor);
 botaoSextou.addEventListener('click', trocaSextou);
 botaoEvento.addEventListener('click', adicionaEvento);
+
+
+
 
